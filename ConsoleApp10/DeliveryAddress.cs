a0c277acd14425cd9d763d316629f704f2012135
Ecc <Ecc@DESKTOP-E983V9F>
2026-09-12 21:57:38 +0300

    Add project files.

---
+++ ConsoleApp10/DeliveryAddress.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmartDeliveryManagementSystem
+{
+ 
+    // 1) Create a DeliveryAddress struct
+
+    public struct DeliveryAddress
+    {
+
+        public string City;
+
+
+        public string Street;
+
+        
+        public int BuildingNumber;
+        public DeliveryAddress(string city, string street, int buildingNumber)
+        {
+            City = city;
+            Street = street;
+            BuildingNumber = buildingNumber;
+        }
+
+        public string GetFullAddress()
+        {
+            return $"{BuildingNumber} {Street} Street, {City}";
+        }
+    }
+}
